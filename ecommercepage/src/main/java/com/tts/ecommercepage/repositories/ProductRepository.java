8a8463b4971ab1efcd46ca1918904b7c0fe475f9
Jacob Gartenstein <jake@Jacobs-MacBook-Pro.local>
2020-11-02 20:05:41 -0600

    built controllers

---
+++ ecommercepage/src/main/java/com/tts/ecommercepage/repositories/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.tts.ecommercepage.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends CrudRepository{
+    
+}
